a30297506caac4e11d7edbfcfcb661227df43856
Daniel Brockman <daniel@brockman.se>
2016-07-23 23:28:05 +0200

    register stablecoins in factory

--- contracts/factory.sol
+++ contracts/factory.sol
@@ -1,9 +1,15 @@
 import 'ss.sol';
 
 contract SimpleStablecoinFactory {
-    function newSimpleStablecoin(Feedbase fb, bytes32 rules) returns (SimpleStablecoin) {
+    mapping (uint => SimpleStablecoin) public stablecoins;
+    uint public count;
+
+    function newSimpleStablecoin(Feedbase fb, bytes32 rules)
+        returns (SimpleStablecoin)
+    {
         var ss = new SimpleStablecoin(fb, rules);
         ss.updateOwner(msg.sender);
+        stablecoins[count++] = ss;
         return ss;
     }
 }
