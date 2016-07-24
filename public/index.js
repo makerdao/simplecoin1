9bca0314be41b57cc532d79ca0196e2155411589
Daniel Brockman <daniel@brockman.se>
2016-07-24 05:47:07 +0200

    improvements

--- public/index.js
+++ public/index.js
@@ -1,72 +1,42 @@
 console.warn("Note: A recent Chrome may be needed to run this app")
 
-var web3 = new Web3
-var feedbase, factory
-var state = { loading: true, rules: "Only professionals" }
-
-function update(changes) {
-  render(state = React.addons.update(state, changes))
-}
-
-function render(state) {
-  forEach(document.querySelectorAll("[data-view]"), app => {
-    var name = app.dataset.view
-    if (!(name in window)) throw new Error(`No such view: ${name}`)
-    var View = window[name]
-    ReactDOM.render(React.createElement(View, state), app)
-  })
-}
+function poke() { load(); render(); loop() }
+function loop() { if (app.loop) setTimeout(poke, app.loop) }
 
 onload = () => fetch("/env").then(response => {
-  response.json().then(json => { env = json; load() })
+  response.json().then(json => { app.env = json; poke() })
 })
 
-var Stablecoins = state => React.DOM.div(null,
-  state.coins ? `${state.coins}` : React.DOM.small(null, `(none)`)
-)
+var app = {
+  loading: true,
+  loop: false,
+  rules: "Only professionals",
+}
+
+var web3 = new Web3
+var feedbase
+var stablecoin_factory
 
 function load() {
-  web3.setProvider(new Web3.providers.HttpProvider(env.ETH_RPC_URL))
-  feedbase = find_object("feedbase")
-  factory = find_object("factory", "simple-stablecoin")
-  reload()
+  delete app.loading
+  web3.setProvider(new Web3.providers.HttpProvider(app.env.ETH_RPC_URL))
+
+  feedbase           = find_dapple_object("feedbase")
+  stablecoin_factory = find_dapple_object("factory", "simple-stablecoin")
+
+  app.coin_count = Number(stablecoin_factory.count())
 }
 
-function reload() {
-  if (state.refresh) setTimeout(refresh, state.refresh)
-  if (state.paused) return
-  if (!factory) return
+var Coins = app => React.DOM.div(null,
+  app.coins ? `${app.coins}` : React.DOM.small(null, `(none)`)
+)
 
-  state.coins = Number(factory.count())
-
-  delete state.loading
-  console.log("Application reloaded.")
-  render(state)
-  document.body.style.visibility = "visible"
-}
-
-function find_object(name, module, object) {
-  module = module || name
-  object = object || name
-
-  if (!(module in dapple)) {
-    throw new Error(`Not found: dapple["${module}"]`)
-  }
-
-  var environment = dapple[module].environments[env.APP_ENV]
-  var instance = new dapple[module].class(web3, env.APP_ENV)
-
-  state[name] = environment.objects[object].address
-
-  return instance.objects[object]
-}
-
-function create() {
-  factory.newSimpleStablecoin(
+function create_stablecoin() {
+  stablecoin_factory.newSimpleStablecoin(
     feedbase.address, web3.toHex(rules.value), {
       from: web3.eth.coinbase
     }, (error, tx) => {
-      if (error){
+      if (error) {
         alert(error)
       } else {
         console.log(`Transaction created: ${tx}`)
@@ -76,14 +46,44 @@ function create() {
   )
 }
 
-var RulesTextarea = state => React.DOM.div(null,
+var Rules = app => React.DOM.div(null,
   React.DOM.textarea({
     id: "rules",
     maxLength: 32,
-    value: state.rules,
+    value: app.rules,
     onChange: event => update({ rules: { $set: event.target.value } })
   })
 )
+
+function render() {
+  document.body.style.visibility = "visible"
+  forEach(document.querySelectorAll("[data-render]"), element => {
+    var name = element.dataset.render
+    if (!(name in window)) throw new Error(`No such element: ${name}`)
+    ReactDOM.render(React.createElement(window[name], app), element)
+  })
+}
+
+function find_dapple_object(name, module, object) {
+  module = module || name
+  object = object || name
+
+  if (!(module in dapple)) {
+    throw new Error(`Not found: dapple["${module}"]`)
+  }
+
+  var environment = dapple[module].environments[app.env.APP_ENV]
+  var instance = new dapple[module].class(web3, app.env.APP_ENV)
+
+  app.env[name] = environment.objects[object].address
+
+  return instance.objects[object]
+}
+
+function update(changes) {
+  app = React.addons.update(app, changes)
+  render()
+}
 
 function forEach(array, thunk) {
   return [].forEach.call(array, thunk)
