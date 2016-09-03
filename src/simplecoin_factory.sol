847b78332b4b41236c8e797d2d93962b29ebde1c
rain <rainbeam@riseup.net>
2016-09-03 05:39:25 +0100

    isolate pre-configured rbac

--- src/simplecoin_factory.sol
+++ src/simplecoin_factory.sol
@@ -22,52 +22,45 @@ contract SimplecoinFactory is DSAuthUser {
         bytes32    rules
     ) returns (Simplecoin coin) {
         coin = new Simplecoin(feedbase, rules);
-
-        DSRoleAuth authority = new DSRoleAuth();
-        configureAuthority({ rbac:   authority,
-                             target: coin,
-                             owner:  msg.sender});
-
+        DSRoleAuth authority = new SimpleRoleAuth(coin);
+        coin.updateAuthority(authority, DSAuthModesEnum.DSAuthModes.Authority);
         coins[count++] = coin;
+    }
+}
+
+contract SimpleRoleAuth is DSRoleAuth {
+    // role identifiers
+    uint8 public admin  = 0;
+    uint8 public issuer = 1;
+    uint8 public holder = 2;
+
+    function SimpleRoleAuth(address target) {
+        address owner = msg.sender;
+
+        // owner can do everything by default
+        setUserRole(owner, admin, true);
+        setUserRole(owner, issuer, true);
+        setUserRole(owner, holder, true);
+
+        // == admin
+        setRoleCapability(admin, target, sig("register(ERC20)"), true);
+        setRoleCapability(admin, target, sig("setVault(uint48,address)"), true);
+        setRoleCapability(admin, target, sig("setFeed(uint48,uint24)"), true);
+        setRoleCapability(admin, target, sig("setSpread(uint48,uint)"), true);
+        setRoleCapability(admin, target, sig("setCeiling(uint48,uint)"), true);
+        setRoleCapability(admin, target, sig("unregister(uint48)"), true);
+
+        // == issuer
+        setRoleCapability(issuer, target, sig("issue(uint48,uint)"), true);
+        setRoleCapability(issuer, target, sig("cover(uint48,uint)"), true);
+
+        // == holder
+        setRoleCapability(holder, target, sig("transfer(address,uint)"), true);
+        setRoleCapability(holder, target, sig("transferFrom(address,address,uint)"), true);
     }
 
     function sig(string name) internal returns (bytes4) {
         return bytes4(sha3(name));
     }
 
-    function configureAuthority(
-        DSRoleAuth rbac,
-        address target,
-        address owner
-        )
-    {
-        // role identifiers
-        uint8 admin  = 0;
-        uint8 issuer = 1;
-        uint8 holder = 2;
-
-        // == admin
-        rbac.setRoleCapability(admin, target, sig("register(ERC20)"), true);
-        rbac.setRoleCapability(admin, target, sig("setVault(uint48,address)"), true);
-        rbac.setRoleCapability(admin, target, sig("setFeed(uint48,uint24)"), true);
-        rbac.setRoleCapability(admin, target, sig("setSpread(uint48,uint)"), true);
-        rbac.setRoleCapability(admin, target, sig("setCeiling(uint48,uint)"), true);
-        rbac.setRoleCapability(admin, target, sig("unregister(uint48)"), true);
-
-        // == issuer
-        rbac.setRoleCapability(issuer, target, sig("issue(uint48,uint)"), true);
-        rbac.setRoleCapability(issuer, target, sig("cover(uint48,uint)"), true);
-
-        // == holder
-        rbac.setRoleCapability(holder, target, sig("transfer(address,uint)"), true);
-        rbac.setRoleCapability(holder, target, sig("transferFrom(address,address,uint)"), true);
-
-        // owner can do everything by default
-        rbac.setUserRole(owner, admin, true);
-        rbac.setUserRole(owner, issuer, true);
-        rbac.setUserRole(owner, holder, true);
-
-        // finally, transfer authority to the owner
-        rbac.updateAuthority(owner, DSAuthModesEnum.DSAuthModes.Owner);
-    }
 }
